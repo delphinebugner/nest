a19e04db674296774409355536424dd499d825c0
Delphine Bugner <delphineb@bam.tech>
2021-02-23 12:17:29 +0100

    feat (main) add transform:true

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
   );
   await app.listen(3000);
 }
