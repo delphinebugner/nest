70e07af2dfd598b599224c03c907f6593ec2f450
Delphine Bugner <delphineb@bam.tech>
2021-02-23 18:22:44 +0100

    feat (coffee controller&service) use pagination in findAll

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,18 +8,17 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
-import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
   @Get()
-  findAll(@Query() paginationQuery) {
-    // const { limit, offset } = paginationQuery;
-    return this.coffeesService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery);
   }
   @Get(':id')
   findOne(@Param('id') id: number) {
