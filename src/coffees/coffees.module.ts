0a2edd9e40dc39bcc08181440d4e7240ced08bc3
Delphine Bugner <delphineb@bam.tech>
2021-02-24 14:38:41 +0100

    fix (coffee.module) import correct Event

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
+import { Event } from '../events/entities/event.entity';
 import { Flavor } from './entities/flavor.entity';
 
 @Module({
