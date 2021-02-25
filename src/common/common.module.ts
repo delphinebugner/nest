b5c7206bc1c5c94d6f19932981da00643f0530b1
Delphine Bugner <delphineb@bam.tech>
2021-02-25 16:23:30 +0100

    move (common.module) extract interceptor with no dependancy to put it in app

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,15 +1,8 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import {
-  APP_FILTER,
-  APP_GUARD,
-  APP_INTERCEPTOR,
-  APP_PIPE,
-  Reflector,
-} from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_PIPE, Reflector } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ApiKeyGuard } from './guards/api-key.guard';
-import { WrapResponseInterceptor } from './interceptors/wrap-response.interceptor';
 
 @Module({
   imports: [Reflector, ConfigModule],
@@ -34,7 +27,6 @@ import { WrapResponseInterceptor } from './interceptors/wrap-response.intercepto
           },
         }),
     },
-    { provide: APP_INTERCEPTOR, useClass: WrapResponseInterceptor },
   ],
 })
 export class CommonModule {}
