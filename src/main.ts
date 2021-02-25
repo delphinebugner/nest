6d98a2ca5cd2ecf9e942851667caf561c0dab58a
Delphine Bugner <delphineb@bam.tech>
2021-02-25 16:23:55 +0100

    feat (main) add Timeout & Wrap-Response interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(
+    new TimeoutInterceptor(),
+    new WrapResponseInterceptor(),
+  );
   await app.listen(3000);
 }
 bootstrap();
